9035a89190fb45560fa3591c261bcc41d096c1b3
John W. Miller <john.w.millr@gmail.com>
2017-04-25 15:24:14 -0500

    Move plotting and GUI functions to Visualizations folder

---
+++ Visualization/guiPrinComps.m
@@ -0,0 +1,113 @@
+function guiPrinComps(xBar,V,D,varargin)
+% GUIPRINCOMPS
+%
+%	INPUT
+%       xBar: Mean shape [2*n_landmarks x 1]
+%       V: Principal components (eigenvectors)
+%       D: Shape weights (eigenvalues)
+%       OPTIONAL
+%           show_image: (bool) Display a face image in the background if true
+%
+% John W. Miller
+% 17-Mar-2017
+
+keys = {'show_image'}; default_values = {0};
+[show_image] = parseKeyValuePairs(varargin,keys,default_values);
+
+% Initialization
+n_pcs = 3;
+slider_values = zeros(n_pcs,1);
+faceRegions = getFaceRegions(); % Connecting dots around the face
+
+% Examine variations from individual PCs
+n_variations = 11; % Must be an odd number
+if mod(n_variations,2) == 0
+    n_variations = n_variations + 1;
+end
+var_step = (n_variations-1)/2;
+weights = zeros(var_step,n_variations);
+for n = 1:n_pcs
+    weights(n,:) = sqrt(D(n))*(-var_step:var_step);
+end
+
+% Create some shape variations
+P = V(:,1:n_pcs);
+shapeVariations = repmat(xBar,1,n_variations) + P*weights(1:n_pcs,:);
+
+% Generate an initial shape
+firstShape = generateShape();
+
+%% Initial visualization
+f = figure();
+if show_image    
+    imDir = fullfile(fileparts(which(mfilename)),'/Faces/faces_B');
+    imFile = 'B_49_0.jpg';
+    im = imread(fullfile(imDir,imFile));
+    imshow(im), hold on
+end
+
+% Determine mean shape and plot dimensions
+mew(:,1) = xBar(1:2:end);
+mew(:,2) = xBar(2:2:end);
+xLim = [0.8 0; 0 1.1]*[min(min(shapeVariations(1:2:end,:))) max(max(shapeVariations(1:2:end,:)))]';
+yLim = [0.8 0; 0 1.3]*[min(min(shapeVariations(2:2:end,:))) max(max(shapeVariations(2:2:end,:)))]';
+
+updatePlot(generateShape()); % Initial plot
+
+%% Create the GUI sliders
+[min_val,max_val] = deal(-(n_variations-1)/2,(n_variations-1)/2);
+stepsize = (1/range([min_val max_val]))*[0.5 1]; % min, max
+
+b1 = uicontrol('Parent',f,'Style','slider','Position',[81,75,419,23],...
+    'value',0, 'min',min_val, 'max',max_val, 'callback', @b_callback_pc_slider,...
+    'SliderStep', stepsize);
+
+b2 = uicontrol('Parent',f,'Style','slider','Position',[81,50,419,23],...
+    'value',0, 'min',min_val, 'max',max_val, 'callback', @b_callback_pc_slider,...
+    'SliderStep', stepsize);
+
+b3 = uicontrol('Parent',f,'Style','slider','Position',[81,25,419,23],...
+    'value',0, 'min',min_val, 'max',max_val, 'callback', @b_callback_pc_slider,...
+    'SliderStep', stepsize);
+
+%% Callback function
+    function b_callback_pc_slider(source,event)
+        switch get(source,'position')*[0 1 0 0]'
+            case 75 % PC 1
+                slider_values(1) = get(source,'value');
+            case 50 % PC 2
+                slider_values(2) = get(source,'value');
+            case 25 % PC 3
+                slider_values(3) = get(source,'value');
+        end        
+        updatePlot(generateShape())
+    end   
+
+%% Generate and plot new shapes
+
+    function newShape = generateShape()        
+        % Calculate the weights for the updated shape
+        b = zeros(n_pcs,1);
+        for n_pc = 1:n_pcs
+            b(n_pc,1) = sqrt(D(n_pc))*(slider_values(n_pc));            
+        end
+        
+        % Generate the shape
+        x = xBar + P*b;
+        newShape(:,1) = x(1:2:end);
+        newShape(:,2) = x(2:2:end);
+    end
+
+    function updatePlot(newShape)
+        hold off
+        if show_image imshow(im), hold on, end
+        for i = 1:length(faceRegions)
+            plot(newShape(faceRegions{i},1),newShape(faceRegions{i},2), '.-','linewidth',2,'color','g'), hold on
+        end
+        plot(mew(:,1),mew(:,2),'.','color','k','linewidth',2)
+        set(gca,'xtick',[],'ytick',[])
+        set(gca,'xlim',xLim,'ylim',yLim,'ydir','reverse')
+        title('Move the sliders to change the weights on the first 3 PCs','fontsize',FS)
+    end
+
+end % End of main
---
+++ Visualization/plotLandmarks.m
@@ -0,0 +1,69 @@
+
+function plotLandmarks(landmarks,varargin)
+% PLOTLANDMARKS plots all of the aligned landmarks from an active shapes model
+%
+%	INPUT
+%       landmarks: The aligned landmarks from multiple images
+%                  [2*n_landmarks x n_shapes]
+%       OPTIONAL
+%           show_lines: 
+%           hold:
+%
+%   See also PLACELANDMARKS, ALIGNSHAPES
+%
+% John W. Miller
+% 14-Mar-2017
+
+% Key-value pair varargin
+keys = {'show_lines','hold'}; default_values = {1,0};
+[show_lines,hold_on] = parseKeyValuePairs(varargin,keys,default_values);
+
+% Plot the landmarks for each shape
+n_shapes = size(landmarks,2);
+if hold_on
+    h = gcf;
+else
+    h = figure;
+end
+hold on
+try colors = parula(n_shapes);
+catch
+    colors = hsv(n_shapes);
+end
+for n_shape = 1:n_shapes
+    iShape = [landmarks(1:2:end,n_shape) landmarks(2:2:end,n_shape)];
+    plot(iShape(:,1),iShape(:,2),'o','color', colors(n_shape,:),...
+        'linewidth',2,'markersize',2,'markerfacecolor',colors(n_shape,:))
+end
+
+% Add mean shape to the plot
+if n_shapes > 1
+    meanShape = mean(landmarks,2); % x1, y1, x2, y2, ..., x20, y20
+else
+    meanShape = landmarks;
+end
+ax = plot(meanShape(1:2:end),meanShape(2:2:end),'ro',...
+    'markersize',5,'linewidth',1,'markerfacecolor','k');
+
+% Connect dots on the face (optional)
+if show_lines    
+    faceLabels = getFaceRegions();
+    
+    mew = [meanShape(1:2:end) meanShape(2:2:end)];
+    for i = 1:length(faceLabels)
+        plot(mew(faceLabels{i},1), mew(faceLabels{i},2), 'm-','linewidth',2)
+    end
+end
+
+% Touch up the plot
+if ~hold_on
+    legend(ax,{'Mean shape'},'fontsize',FS,'location','southeast')
+    text(0.05,0.5,sprintf('n=%d',n_shapes),'units','normalized','fontsize',FS,'fontweight','bold')
+end
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+axis off;
+set(gca,'YDir','reverse');
+
+end % End of main
