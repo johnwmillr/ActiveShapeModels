2ee8215970b452680c2afac7576092bb913406e0
John W. Miller <john.w.millr@gmail.com>
2017-04-28 16:24:26 -0500

    Face detection improving, but not quite there...

--- asm_multiResolution.m
+++ asm_multiResolution.m
@@ -1,4 +1,4 @@
-function x_aligned = asm_multiResolution(im_original,alignedShapes)
+function [x_aligned,h] = asm_multiResolution(im_original,xBar,h_filt)
 % ASM_MULTIRESOLUTION 
 %
 %	INPUT
@@ -11,60 +11,56 @@ function x_aligned = asm_multiResolution(im_original,alignedShapes)
 % John W. Miller
 % 12-Apr-2017
 
+% impyramid()
 
 %%
 % imDir = './Faces/faces_B';
 % imFile = 'B_40_0.jpg';
 % im_original = imread(fullfile(imDir,imFile));
 
-%% Downsample
-n = 8;
-im = double(im_original(1:n:end,1:n:end));
-
 % Filter (smooth image)
-A = 1;
-sig = .3; % Need to play w/ these parameters
-mew = 1;
-x = (-3*sig+mew):0.1:(3*sig+mew);
-h = A*exp(-((x-mew).^2)./(2*sig^2));
-im_filt = conv2(h,h,im,'same');
-% figure,imshow(im_filt,[])
+if nargin < 3
+    A = 1;
+    sig = .3; % Need to play w/ these parameters
+    mew = 1;
+    x = (-3*sig+mew):0.1:(3*sig+mew);
+    h_filt = A*exp(-((x-mew).^2)./(2*sig^2));
+end
+im_filt = conv2(h_filt,h_filt,im_original,'same');
+
+% Downsample
+n = 1;
+im = double(im_filt(1:n:end,1:n:end));
 
 % Convolve with head shape
 hair_shape = ones(round(size(im).*[1/5 1/3]));
-hair_response = conv2(1./im_filt,hair_shape,'same');
-rc = 8;
+hair_response = conv2(1./im,hair_shape,'same');
+rc = 8; % rows and columns to cut off the edge off the image (Kludge)
 hair_response(:,1:rc) = 0;
 hair_response(:,end-rc:end) = 0;
 hair_response(1:rc,:) = 0;
 hair_response(end-rc:end,:) = 0;
 [~,idx] = max(hair_response(:));
 [I,J] = ind2sub(size(im),idx);
-% figure(1), imshow(im,[]); hold on
-% plot(J,I,'ro'), hold off
 
 % Determine where to place mean shape
-%
-figure(1), imshow(im_original,[]), hold on
-plot(J*n,I*n,'ro')
-I = I + round(size(hair_shape,2)/2); % KLUDGE (try to move from hair to eyes)
-plot(J*n,I*n,'ro')
-x = mean(alignedShapes,2);
+%figure, imshow(im_original,[]), hold on
+% plot(J*n,I*n,'ro')
+I = I + round(size(hair_shape,2)/2.5); % KLUDGE (try to move from hair to eyes)
+%plot(J*n,I*n,'ro')
 
-[x1,y1] = deal(x(17),x(18));
-[x2,y2] = deal(x(19),x(20));
+[x1,y1] = deal(xBar(17),xBar(18));
+[x2,y2] = deal(xBar(19),xBar(20));
 [xM, yM] = deal(mean([x1 x2]),mean([y1 y2])); % Top middle of face
-subMean = zeros(size(x));
+subMean = zeros(size(xBar));
 subMean(1:2:end) = xM;
 subMean(2:2:end) = yM;
-x_aligned = x-subMean;
+x_aligned = xBar-subMean;
 x_aligned(1:2:end) = x_aligned(1:2:end) + J*n; % Scale back up the detected region and shift
 x_aligned(2:2:end) = x_aligned(2:2:end) + I*n;
 
 % Add mean shape to image
-plotLandmarks(x_aligned,'show_lines',1,'hold_on',1)
-
-
-
+figure, imshow(im_original,[])
+h = plotLandmarks(x_aligned,'show_lines',1,'hold',1);
 
 end % End of main
