f61c7e19ad9e1c89d53a8c20ddc2682f31f6fe9b
John W. Miller <john.w.millr@gmail.com>
2017-05-15 13:24:23 -0500

    Add method to view the 2D profiles from a gray model

---
+++ Visualization/viewGrayProfile.m
@@ -0,0 +1,20 @@
+function viewGrayProfile(grayModel,nr,nl)
+% VIEWGRAYPROFILE 
+%
+%	INPUT
+%       grayModel: Comes from BUILDGRAYLEVELMODEL.
+%       nr: resolution number
+%       nl: landmark number
+%
+%   See also BUILDGRAYLEVELMODEL
+%
+% John W. Miller
+% 15-May-2017
+
+% Reshape from vector to square
+graySquare = reshape(grayModel(nr).meanProfile{nl},grayModel(nr).Info.rc_squaresize-1,grayModel(nr).Info.rc_squaresize-1);
+
+% View the square region
+figure, imshow(graySquare,[])
+
+end % End of main
